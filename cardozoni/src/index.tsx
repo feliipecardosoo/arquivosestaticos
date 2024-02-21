8ce3473784c45a2180e3e3cf015aa1357058c26d
feliipecardosoo <fglibino@gmail.com>
2024-02-21 20:34:28 -0300

    a

--- cardozoni/src/index.tsx
+++ cardozoni/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Cardapio from './pages/cardapio';
 
 
 
@@ -9,6 +10,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <div> Cardozoni </div>
+    <Cardapio />
   </React.StrictMode>
 );
---
+++ cardozoni/src/pages/cardapio/index.tsx
@@ -0,0 +1,11 @@
+import style from './Cardapio.module.scss'
+
+export default function Cardapio() {
+    return (
+        <main>
+            <nav className={style.menu}>
+                <img src="../../assets/img.svg" alt="Logo Cardozoni" />
+            </nav>
+        </main>
+    )
+}
