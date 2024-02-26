63cc121504571baed2d802f0d4c7056a68f585da
feliipecardosoo <fglibino@gmail.com>
2024-02-26 20:43:53 -0300

    a

--- cardozoni/src/pages/cardapio/index.tsx
+++ cardozoni/src/pages/cardapio/index.tsx
@@ -1,36 +1,34 @@
-import style from './Cardapio.module.scss'
-import {ReactComponent as Logo} from 'assets/img.svg'
-import Buscador from './buscador'
-import { useState } from 'react'
-import Filtros from './filtros'
-import Ordenador from './ordenador'
+import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
+import Buscador from "./Buscador";
+import { useState } from "react";
+import Filtros from "./Filtros";
+import Ordenador from "./Ordenador";
+import Itens from './Itens';
 
 export default function Cardapio() {
-    const [busca, setBusca] = useState('')
-    const [filtro, setFiltro] = useState <number | null>(null)
-    return (
-        <main>
-            <nav className={style.menu}>
-                <Logo />
-            </nav>
-            <header className={style.header}>
-                <div className={style.header__text}>
-                    A casa do código e da massa                   
-                </div>
-            </header>
-            <section className={style.cardapio}>
-                <h3 className={style.cardapio__titulo}>Cardapio</h3>
-                <Buscador 
-                    busca={busca} 
-                    setBusca={setBusca}/>
-                    <div className={style.cardapio__filtros}>
-                        <Filtros
-                        filtro = {filtro}
-                        setFiltro = {setFiltro}
-                        />
-                        <Ordenador/>
-                    </div>
-            </section>
-        </main>
-    )
+  const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState("");
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
+          A casa do código e da massa
+        </div>
+      </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+        <Buscador busca={busca} setBusca={setBusca} />
+        <div className={styles.cardapio__filtros}>
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+        </div>
+        <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
+      </section>
+    </main>
+  )
 }
--- cardozoni/src/pages/cardapio/ordenador/index.tsx
+++ cardozoni/src/pages/cardapio/ordenador/index.tsx
@@ -1,17 +1,41 @@
-import style from './ordenador.module.scss'
-import opcoes from './opcoes.json'
+import styles from "./Ordenador.module.scss";
+import opcoes from "./opcoes.json";
+import React, { useState } from "react";
+import classNames from "classnames";
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md';
 
-export default function Ordenador() {
-    return (
-        <button className={style.ordenador}>
-            <span>Ordenador Por</span>
-            <div className={style.ordenador__options}>
-                {opcoes.map(opcao => (
-                    <div className={style.ordenador__option} key={opcao.value}>
-                       {opcao.nome} 
-                    </div>
-                ))}
-            </div>
-        </button>
-    )
+interface Props { 
+  ordenador: string,
+  setOrdenador: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function Ordenador({
+  ordenador,
+  setOrdenador
+}: Props) {
+  const [aberto, setAberto] = useState(false);
+  const nomeOrdenador = ordenador && opcoes.find(opcao => opcao.value === ordenador)?.nome;
+  return (
+    <button
+      className={classNames({
+        [styles.ordenador]: true,
+        [styles["ordenador--ativo"]]: ordenador !== ""
+      })}
+      onClick={() => setAberto(!aberto)}
+      onBlur={() => setAberto(false)}
+    >
+      <span>{nomeOrdenador || "Ordenar Por"}</span>
+      {aberto ? <MdKeyboardArrowUp size={20} /> : <MdKeyboardArrowDown size={20} /> }
+      <div className={classNames({
+        [styles.ordenador__options]: true,
+        [styles['ordenador__options--ativo']]: aberto
+      })}>
+        {opcoes.map(opcao => (
+          <div className={styles.ordenador__option} key={opcao.value} onClick={() => setOrdenador(opcao.value)}>
+            {opcao.nome}
+          </div>
+        ))}
+      </div>
+    </button>
+  )
 }
