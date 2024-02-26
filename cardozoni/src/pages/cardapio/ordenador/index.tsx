dbe233c6415c5a1e560159646cc4ea5611bf1b15
feliipecardosoo <fglibino@gmail.com>
2024-02-26 20:26:41 -0300

    a

---
+++ cardozoni/src/pages/cardapio/ordenador/index.tsx
@@ -0,0 +1,17 @@
+import style from './ordenador.module.scss'
+import opcoes from './opcoes.json'
+
+export default function Ordenador() {
+    return (
+        <button className={style.ordenador}>
+            <span>Ordenador Por</span>
+            <div className={style.ordenador__options}>
+                {opcoes.map(opcao => (
+                    <div className={style.ordenador__option} key={opcao.value}>
+                       {opcao.nome} 
+                    </div>
+                ))}
+            </div>
+        </button>
+    )
+}
